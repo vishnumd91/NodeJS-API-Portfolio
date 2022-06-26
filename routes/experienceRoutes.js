c2f2c567fbdeb4ce382c178eb6289e2cb607e898
Vishnu MD <vishnudivakaran1@gmail.com>
2022-06-26 20:50:51 +0530

    CRUD operaations implemented for experience module (#1)

--- routes/experienceRoutes.js
+++ routes/experienceRoutes.js
@@ -2,12 +2,21 @@ const express = require("express");
 const {
   getExperienceData,
   addExperienceData,
+  getExperienceDataById,
+  updateExperienceData,
+  deleteExperienceData,
 } = require("../controllers/experienceController");
 
 const router = express.Router();
 
+router.post("/experience", addExperienceData);
+
+router.get("/experience/:id", getExperienceDataById);
+
 router.get("/experience", getExperienceData);
 
-router.post("/experience", addExperienceData);
+router.put("/experience/:id", updateExperienceData);
+
+router.delete("/experience/:id", deleteExperienceData);
 
 module.exports = router;
