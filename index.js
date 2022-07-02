d1da2034a4ff5b78175977ae94495b512117794b
vishnumd91 <vishnudivakaran1@gmail.com>
2022-07-02 20:39:03 +0530

    static route issue fix for serving routes in UI

--- index.js
+++ index.js
@@ -29,10 +29,10 @@ app.use("/", (req, res) => {
   res.send("Hello, Welcome to Portfolio API");
 });
 
-app.use(express.static(path.join(__dirname, "/build")));
+app.use(express.static(path.join(__dirname, "build")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname + "/build/", "index.html"));
+  res.sendFile(path.resolve(__dirname, "build", "index.html"));
 });
 
 app.listen(PORT, () => console.log(`Server running on PORT ${PORT}`));
